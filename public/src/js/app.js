546262f980f08ad74c2c33617f098c096e0b525d
PagePetty <pagevpetty@live.com>
2019-05-29 20:25:01 -0600

    6. (45. Promises)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -13,8 +13,43 @@ window.addEventListener('beforeinstallprompt', function (event) {
     deferredPrompt = event;
     return false;
 });
-setTimeout(function () {
-    console.log('This is executed once the timer is done!')
-}, 3000);
+
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('this is executed once the timer is done!');
+        //console.log('This is executed once the timer is done!');
+    }, 3000);
+
+});
+
+promise.then(function (text) {
+    return text;
+}).then(function (newText) {
+    console.log(newText);
+});
 
 console.log('This is executed right after setTimeout()');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
