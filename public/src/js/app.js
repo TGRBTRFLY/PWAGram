466810993c38613ebedcf6d8a6756e171d6141d3
PagePetty <pagevpetty@live.com>
2019-06-01 13:57:25 -0600

    61. Adjusting the Course Project - Cashing

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,4 @@
+
 var deferredPrompt;
 
 if (!window.Promise) {
@@ -8,97 +9,16 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
         .then(function () {
-            console.log('Service Worker registered!');
+            console.log('Service worker registered!');
         })
         .catch(function (err) {
             console.log(err);
         });
 }
+
 window.addEventListener('beforeinstallprompt', function (event) {
-    console.log('beforeinstallprompt');
+    console.log('beforeinstallprompt fired');
     event.preventDefault();
     deferredPrompt = event;
     return false;
 });
-
-var promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        //resolve('this is executed once the timer is done!');
-        reject({code: 500, message: 'An error occurred!'});
-        //console.log('This is executed once the timer is done!');
-    }, 3000);
-});
-
-fetch('https://httpbin.org/ip')
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    requestHeaders: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify({message: 'Does this work?'})
-})
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
-
-// promise.then(function (text) {
-// //     return text;
-// // }, function (err) {
-// //     console.log(err.code, err.message)
-// //
-// // }).then(function (newText) {
-// //     console.log(newText);
-// // });
-
-promise.then(function (text) {
-    return text;
-}).then(function (newText) {
-    console.log(newText);
-}).catch(function (err) {
-    console.log(err.code, err.message);
-});
-console.log('This is executed right after setTimeout()');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
